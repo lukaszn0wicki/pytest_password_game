ef17ebe4d84938367b84cff5d837ca3937c7fb89
lukaszn0wicki <46539935+lukaszn0wicki@users.noreply.github.com>
2024-04-07 14:25:06 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ import random
 import string
 import time
 from datetime import datetime
-from playwright.sync_api import (sync_playwright)
+from playwright.sync_api import sync_playwright
 from selectors import Selectors
 
 
